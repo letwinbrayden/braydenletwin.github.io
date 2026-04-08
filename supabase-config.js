8543d5c34094403f0176074b05cd566134e45f3f
Brayden Letwin <106283242+letwinbrayden@users.noreply.github.com>
2026-04-07 23:09:58 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'YOUR_PROJECT_URL';
-export const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+export const SUPABASE_URL = 'https://qpcsabadqulbchnpguab.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_NWdA4gb9I5RF3Uevq0uM_Q_4cqpsh-z';
